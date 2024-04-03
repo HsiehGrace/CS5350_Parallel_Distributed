fabff998a78e63a002c9ca5677b7ea63be686fbf
Sean1783 <86225444+Sean1783@users.noreply.github.com>
2024-04-03 15:07:21 -0700

    1D parallel matrix multiplication. Change values of m, n, q, p to test.

--- CS5350_Parallel_Distributed/CS5350_Parallel_Distributed.cpp
+++ CS5350_Parallel_Distributed/CS5350_Parallel_Distributed.cpp
@@ -1,113 +1,154 @@
-#include <stdio.h>
+#include <omp.h>
 #include <iostream>
+#include <time.h>      
+#include <chrono>
 #include <vector>
-#include "omp.h" //OpenMP
 
-// Prototypes
-// Utility functions
-void print_2D_vector(std::vector<std::vector<int>> v);
-void print_1D_vector(std::vector<int> v);
-std::vector<std::vector<int>> random_2D_vector(int rows, int cols, int max_val = 5);
+typedef std::vector<std::vector<int>> Matrix;
 
-// Matrix Multiplications
-std::vector<std::vector<int>> sequential_MM(std::vector<std::vector<int>> matrixA, std::vector<std::vector<int>> matrixB, int ditto);
+Matrix create_identity_matrix(const int &m, const int &n);
+Matrix create_test_matrix(const int &m, const int &n);
+Matrix MM_1D_parallel(const Matrix &mtx_A, const Matrix &mtx_B, const int &p);
+Matrix MM_sequential(const Matrix &mtx_A, const Matrix &mtx_B);
+void print_matrix(const Matrix &mtx);
+void verify(const Matrix &mtx_C, const Matrix &control_mtx);
 
-int main()
-{
-    // Check that OpenMP works for your PC
-    /*
-    #pragma omp parallel num_threads(4)
-        {
-            printf("Hello from thread %d, nthreads %d\n", omp_get_thread_num(), omp_get_num_threads());
-        }
-        */
+const bool TIMER = true;
+const bool DEBUG = false;
+
+int main (int argc, char* argv[]) {
+
+  // Please use powers of 2 for all values or it doesn't work. 
+  const int m = 512;
+  const int n = 512;
+  const int q = 256;
+  const int p = 8;
 
 
-    // Create matricies
-    int rows_A = 3;        // m
-    int ditto_val = 2;      // n
-    int cols_B = 2;         // q
+  Matrix matrix_A = create_identity_matrix(m, n);
+  Matrix matrix_B = create_test_matrix(n, q);
+  Matrix matrix_C = MM_1D_parallel(matrix_A, matrix_B, p);
+  Matrix matrix_D = MM_sequential(matrix_A, matrix_B);
+  verify(matrix_C, matrix_D);
 
-    std::vector<std::vector<int>> matrixA = random_2D_vector(rows_A, ditto_val); // m x n
-    std::vector<std::vector<int>> matrixB = random_2D_vector(ditto_val, cols_B); // n x q
-    std::vector<std::vector<int>> matrixC;                                       // m x q
 
-    // MM Algorithms
-    matrixC = sequential_MM(matrixA, matrixB, ditto_val);
-    print_2D_vector(matrixA);
-    print_2D_vector(matrixB);
-    print_2D_vector(matrixC);
+  return 0;
 
-    return 0;
 }
 
-// Print out all the contents of vector (v)
-void print_2D_vector(std::vector<std::vector<int>> v)
-{
-    for (int i = 0; i < v.size(); i++)
-    {
-        for (int j = 0; j < v[i].size(); j++)
-            std::cout << v[i][j] << " ";
 
-        std::cout << std::endl;
-    }
-
-    std::cout << std::endl;
-}
-
-// Print out all the contents of a nested/2D vector (v)
-void print_1D_vector(std::vector<int> v)
-{
-    for (int i = 0; i < v.size(); i++)
-        std::cout << v[i] << " ";
-
-    std::cout << std::endl;
-}
-
-// Create a nested/2D vector with random integers from 0 to max_val - 1
-std::vector<std::vector<int>> random_2D_vector(int rows, int cols, int max_val)
-{
-    std::vector<std::vector<int>> v;
-
-    if (max_val == 0)
-        max_val = 1;
-
-    for (int i = 0; i < rows; i++)
-    {
-        v.push_back({});
-        for (int j = 0; j < cols; j++)
-        {
-            v[i].push_back(rand() % max_val);
-        }
-    }
-
-    return v;
-}
-
-/*
-* You will implement a serial MM algorithm using three nested for loops. 
-* This implementation will be used to conduct the performance study. 
-*/
-std::vector<std::vector<int>> sequential_MM(std::vector<std::vector<int>> matrixA, std::vector<std::vector<int>> matrixB, int ditto)
-{
-    // Initilize Vector (matrix)
-
-    // A (m x n) * B (n x q) = C (m x q)
-    int answerRows = matrixA.size(), answerCols = matrixB[0].size();
-    std::vector<std::vector<int>> answer = random_2D_vector( answerRows, answerCols, 1);
-    
-    for (int i = 0; i < answerRows; i++)
-    {
-        for (int j = 0; j < answerCols; j++)
-        {
-            int value_at_location = 0;
-            for (int k = 0; k < ditto; k++)
-            {
-                value_at_location += matrixA[i][k] * matrixB[k][j];
-            }
-            answer[i][j] = value_at_location;
-        }
-    }
+Matrix MM_sequential(const Matrix &mtx_A, const Matrix &mtx_B) {
+  int m = mtx_A.size();
+  int n = mtx_B.size();
+  int q = mtx_B[0].size();
+  Matrix result(m, std::vector<int>(q, 0));
   
-    return answer;
+  auto start = std::chrono::high_resolution_clock::now();
+  
+  for (int i = 0; i < m; ++i) {
+    for (int j = 0; j < q; ++j) {
+      int sum = 0;
+      for (int k = 0; k < n; ++k) {
+        sum += mtx_A[i][k] * mtx_B[k][j];
+      }
+      result[i][j] = sum;
+    }
+  }
+
+  if (TIMER) {
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration = end - start;
+    std::cout << "Sequential execution time: " << duration.count() << " seconds" << std::endl;
+  }
+
+
+  return result;
+}
+
+
+Matrix MM_1D_parallel(const Matrix &mtx_A, const Matrix &mtx_B, const int &p) {
+
+  int m = mtx_A.size();
+  int n = mtx_B.size();
+  int q = mtx_B[0].size();
+  Matrix result(m, std::vector<int>(q));
+
+  omp_set_num_threads(p);
+  
+  auto start = std::chrono::high_resolution_clock::now();
+  
+  #pragma omp parallel 
+  {
+    int r = omp_get_thread_num();
+
+    // #pragma omp critical 
+    // {
+    //   std::cout << "Hello from thread : " << r << "\n";
+    // }
+    
+    for (int i = 0; i < m/p; i ++) {
+      for (int j = 0; j < q; j++) {
+        int sum = 0;
+        for (int k = 0; k < n; k++) {
+          sum += mtx_A[i + r*(m/p)][k] * mtx_B[k][(j + r) % q];
+        }
+        result[i + r*(m/p)][(j + r) % q] = sum;
+      }
+    }
+  }
+
+  if (TIMER) {
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration = end - start;
+    std::cout << "Parallel execution time: " << duration.count() << " seconds" << std::endl;
+  }
+  return result;
+}
+
+void verify(const Matrix &mtx_C, const Matrix &control_mtx) {
+  int errors = 0;
+  int m = mtx_C.size();
+  int n = mtx_C[0].size();
+  for (int i = 0; i < m; i++) {
+    for (int j = 0; j < n; j++) {
+      if (mtx_C[i][j] != control_mtx[i][j]) {
+        if (DEBUG) {
+          std::cout << "Failed at [" << i << "][" << j << "]\n";
+        }
+        errors++;
+      }
+    }
+  }
+  std::cout << "Test complete. " << errors << " errors found.\n";
+}
+
+
+Matrix create_identity_matrix(const int &m, const int &n) {
+  Matrix result(m, std::vector<int>(n));
+  for (int i = 0; i < m; i++) {
+    for (int j = 0; j < n; j++) {
+      result[i][j] = (i == j ? 1 : 0);
+    }
+  }
+  return result;
+}
+
+Matrix create_test_matrix(const int &m, const int &n) {
+  Matrix result(m, std::vector<int>(n));
+  srand (time(NULL));
+  for (int i = 0; i < m; i++) {
+    for (int j = 0; j < n; j++) {
+      result[i][j] = rand() % 5 + 1;
+    }
+  }
+  return result;
+}
+
+void print_matrix(const Matrix &mtx) {
+  for (int i = 0; i < mtx.size(); i++) {
+    for (int j = 0; j < mtx[0].size(); j++) {
+      std::cout << mtx[i][j] << " ";
+    }
+    std::cout << "\n";
+  }
 }
